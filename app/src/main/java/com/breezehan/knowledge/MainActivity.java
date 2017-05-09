bca3ae04766ab1c4c15555e33415b64dc6cc4bb1
breezehan <handongsheng2008@126.com>
2017-05-09 17:13:18 +0800

    Intent的简单隐式写法;；更新buildtools等。

--- app/src/main/java/com/breezehan/knowledge/MainActivity.java
+++ app/src/main/java/com/breezehan/knowledge/MainActivity.java
@@ -1,13 +1,30 @@
 package com.breezehan.knowledge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+/**
+ * Description 对应博客中的文章http://blog.csdn.net/hds2011/article/details/71215298
+ * Author  breezehan
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("com.breezehan.knowledge.second");
+                intent.addCategory("com.breezehan.knowledge.mycategory");
+                startActivity(intent);
+            }
+        });
     }
 }
