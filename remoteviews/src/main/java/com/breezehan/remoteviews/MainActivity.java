ca98e1e56717490bdc434a94d6caedd7b69de09c
breezehan <handongsheng2008@126.com>
2017-08-24 20:56:02 +0800

    准备讨论remoteViews机制

--- remoteviews/src/main/java/com/breezehan/remoteviews/MainActivity.java
+++ remoteviews/src/main/java/com/breezehan/remoteviews/MainActivity.java
@@ -1,13 +1,44 @@
 package com.breezehan.remoteviews;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.RemoteViews;
+/**
+ * Author  hands
+ * Description  解析原理使用，关于Notification更多请看其他:
+ *              如1.http://iluhcm.com/2017/03/12/experience-of-adapting-to-android-notifications/
+ *              2.http://www.jianshu.com/p/22e27a639787
+ * Date    2017/8/24 17:18
+ * Version
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void fireNotification(View view) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("通知")
+                .setContentText("Hello world!");
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, NotificationActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+//        builder.setContentIntent(pendingIntent);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.notification_layout);
+        builder.setContent(remoteViews);
+        remoteViews.setTextViewText(R.id.msg,"通知通知...");
+        remoteViews.setImageViewResource(R.id.icon,R.mipmap.ic_launcher);
+        remoteViews.setOnClickPendingIntent(R.id.open_activity2,pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID,builder.build());
     }
 }
--- remoteviews/src/main/java/com/breezehan/remoteviews/MyAppWidgetProvider.java
+++ remoteviews/src/main/java/com/breezehan/remoteviews/MyAppWidgetProvider.java
@@ -1,6 +1,19 @@
 package com.breezehan.remoteviews;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
 
 /**
  * Author  hands
@@ -10,4 +23,69 @@ import android.appwidget.AppWidgetProvider;
  */
 
 public class MyAppWidgetProvider extends AppWidgetProvider {
+    public static final String TAG = "MyAppWidgetProvider";
+    public static final String CLICK_ACTION = "com.breezehan.remoteviews.action.CLICK";
+
+    public MyAppWidgetProvider() {
+        super();
+    }
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        super.onReceive(context, intent);
+        Log.i(TAG, "onReceive: action = " + intent.getAction());
+        if (intent.getAction().equals(CLICK_ACTION)) {
+            Toast.makeText(context, "clicked it", Toast.LENGTH_LONG).show();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Bitmap srcBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.icon);
+                    AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+                    for (int i = 0; i < 37; i++) {
+                        float degree = (i * 10) % 360;
+                        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+                        remoteViews.setImageViewBitmap(R.id.imgView_widget, rotateBitmap(context, srcBitmap, degree));
+                        Intent intentClick = new Intent();
+                        intentClick.setAction(CLICK_ACTION);
+                        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentClick, 0);
+                        remoteViews.setOnClickPendingIntent(R.id.imgView_widget,pendingIntent);
+                        appWidgetManager.updateAppWidget(new ComponentName(context,MyAppWidgetProvider.class),remoteViews);
+                        SystemClock.sleep(30);
+                    }
+                }
+            }).start();
+        }
+    }
+
+    //每次小部件更新都会调用此方法
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.i(TAG, "onUpdate:");
+        int counter = appWidgetIds.length;
+        Log.i(TAG, "counter=" + counter);
+        for (int i = 0; i < counter; i++) {
+            int appWidgetId = appWidgetIds[i];
+            onWidgetUpdate(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void onWidgetUpdate(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        Log.i(TAG, "appWidgetId = "+appWidgetId);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+        //"桌面小部件"发送的广播
+        Intent intentClick = new Intent();
+        intentClick.setAction(CLICK_ACTION);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intentClick, 0);
+        remoteViews.setOnClickPendingIntent(R.id.imgView_widget, pendingIntent);
+        appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+    }
+
+    private Bitmap rotateBitmap(Context context, Bitmap srcBitmap, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.reset();
+        matrix.setRotate(degree);
+        Bitmap tmpBitmap = Bitmap.createBitmap(srcBitmap, 0, 0, srcBitmap.getWidth(), srcBitmap.getHeight(), matrix, true);
+        return tmpBitmap;
+    }
 }
