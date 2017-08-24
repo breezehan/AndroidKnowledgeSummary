2ed4936a3b6649b9e697fb48db11a5b9fc290396
breezehan <handongsheng2008@126.com>
2017-08-24 20:54:55 +0800

    'RemoteViews原理'

---
+++ remoteviews/src/main/java/com/breezehan/remoteviews/MyAppWidgetProvider.java
@@ -0,0 +1,13 @@
+package com.breezehan.remoteviews;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Author  hands
+ * Description
+ * Date    2017/8/24 17:56
+ * Version
+ */
+
+public class MyAppWidgetProvider extends AppWidgetProvider {
+}
